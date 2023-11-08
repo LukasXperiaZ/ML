e73a40cea4c15703c380b068c29eb5a0e072c17f
Lukas <e11911069@student.tuwien.ac.at>
2023-11-08 18:10:00 +0100

    Deleted unnecessary code in DecisionTree.py

--- Ex1/Loan/DecisionTree.py
+++ Ex1/Loan/DecisionTree.py
@@ -8,20 +8,14 @@ from sklearn import metrics  # Import scikit-learn metrics module for accuracy c
 # Note: Decision Trees do not require scaling/normalization
 def classify():
     # load train data
-    loan_lrn = pd.read_csv("../../datasets/Loan/loan-10k.lrn.csv")
-    print(loan_lrn.head())
-    column_names = loan_lrn.columns.to_list()
+    loan_lrn = pd.read_csv("../../datasets/Loan/loan-10k.lrn.csv", engine='python')
 
-    loan_lrn = pd.read_csv("../../datasets/Loan/loan-10k.lrn.csv", skiprows=1, header=None,
-                           names=column_names, engine='python')
-
-    features = column_names.copy()
+    features = loan_lrn.columns.to_list()
     del features[91]
     del features[0]
     x = loan_lrn[features]
     # do one-hot encoding for all categorical variables (sklearn cannot handle categorical variables),
     x = pd.get_dummies(x, drop_first=True)  # it would require some defined order for every categorical variable!
-
     y = loan_lrn.grade
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)  # 70% train, 30% test
