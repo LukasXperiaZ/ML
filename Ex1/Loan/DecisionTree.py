1ac5c0c397fcbb5ed4a7c8fe77aa79e941faeb33
Lukas <e11911069@student.tuwien.ac.at>
2023-11-09 15:07:40 +0100

    Added plot and played with different settings

--- Ex1/Loan/DecisionTree.py
+++ Ex1/Loan/DecisionTree.py
@@ -1,8 +1,14 @@
 # Load libraries
+import time
+
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier  # Import Decision Tree Classifier
 from sklearn.model_selection import train_test_split  # Import train_test_split function (not needed)
 from sklearn import metrics  # Import scikit-learn metrics module for accuracy calculation
+from sklearn.tree import export_graphviz
+from six import StringIO
+from IPython.display import Image
+import pydotplus
 
 
 # Note: Decision Trees do not require scaling/normalization
@@ -16,22 +22,37 @@ def classify():
     x = loan_lrn[features]
     # do one-hot encoding for all categorical variables (sklearn cannot handle categorical variables),
     x = pd.get_dummies(x, drop_first=True)  # it would require some defined order for every categorical variable!
+    features = x.columns.to_list()
     y = loan_lrn.grade
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)  # 70% train, 30% test
 
     # Create Decision Tree classifier Object
-    clf = DecisionTreeClassifier()
+    # criterion: "gini" (gini index), "log_loss", "entropy" (information gain)
+    clf = DecisionTreeClassifier(criterion="log_loss", max_depth=2)
 
+    start = time.time()
     # Train Decision Tree Classifier
     clf = clf.fit(x_train, y_train)
+
+    end = time.time()
+    elapsed_time = end - start
 
     # Predict the response for the test dataset
     y_pred = clf.predict(x_test)
 
     # Model Accuracy, how often is the classifier correct?
     accuracy = metrics.accuracy_score(y_test, y_pred)
-    print(accuracy)
+    print("Accuracy: " + str(accuracy))
+    print("Time: " + str(elapsed_time))
+
+    dot_data = StringIO()
+    export_graphviz(clf, out_file=dot_data, filled=True, rounded=True,
+                    special_characters=True, feature_names=features, class_names=['A', 'B', 'C', 'D', 'E', 'F', 'G'])
+    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+    if graph is not None:
+        graph.write_png('./plots/loan.png')
+        Image(graph.create_png())
 
 
 if __name__ == '__main__':
