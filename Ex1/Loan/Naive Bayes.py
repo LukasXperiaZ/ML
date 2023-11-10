4fd6bb6156fd8f1485edb7e01a85f4bde0bde6e7
Lukas <e11911069@student.tuwien.ac.at>
2023-11-10 15:09:22 +0100

    Tried to improve this approach: https://medium.com/analytics-vidhya/naive-bayes-for-mixed-typed-data-in-scikit-learn-fb6843e241f0 But can't get it to work, will stay with the one-hot encoding.

--- Ex1/Loan/Naive Bayes.py
+++ Ex1/Loan/Naive Bayes.py
@@ -1,7 +1,9 @@
 import time
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split  # Import train_test_split function
-from sklearn.naive_bayes import GaussianNB, CategoricalNB
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, CategoricalNB
 from sklearn.metrics import (
     accuracy_score,
     confusion_matrix,
@@ -24,8 +26,6 @@ def classify():
     x = loan_lrn[features]
     y = loan_lrn.grade
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)  # 70% train, 30% test
-
     numeric_features = x.drop(
         ['term', 'emp_length', 'home_ownership', 'verification_status', 'loan_status',
          'pymnt_plan', 'purpose', 'addr_state', 'initial_list_status', 'application_type',
@@ -37,13 +37,27 @@ def classify():
                             'pymnt_plan', 'purpose', 'addr_state', 'initial_list_status', 'application_type',
                             'hardship_flag', 'debt_settlement_flag', 'disbursement_method']
 
+    # Label encode the categorical features (a label is a number)
+    x_cat = x[categorical_features]
+    le = LabelEncoder()
+    x_cat_encoded = x_cat.apply(le.fit_transform)
+    x_numeric_features = x[numeric_features]
+
+    # We have to reset the indices to be able to concatenate
+    x_cat_encoded.reset_index(drop=True, inplace=True)
+    x_numeric_features.reset_index(drop=True, inplace=True)
+    x = pd.concat([x_numeric_features, x_cat_encoded], axis=1)
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)  # 70% train, 30% test
+
+    # Based on this: https://stackoverflow.com/questions/14254203/mixing-categorial-and-continuous-data-in-naive-bayes-classifier-using-scikit-lea
     # Build a Gaussian Classifier for continuous data
     model_cont = GaussianNB()
     # get the subset of the training set containing only continuous/numeric values
     x_train_cont = x_train[numeric_features]
 
     # Build a Categorical Classifier for categorical data
-    model_cat = CategoricalNB()
+    model_cat = MultinomialNB()
     # get the subset of the training set containing only categorical values
     x_train_cat = x_train[categorical_features]
 
@@ -62,12 +76,15 @@ def classify():
     x_test_cat = x_test[categorical_features]
 
     # Predict the log probabilities for both models
-    log_prob_cont = model_cont.predict_log_proba(x_test_cont)
-    log_prob_cat = model_cat.predict_log_proba(x_test_cat)
-    log_prob_total = log_prob_cont + log_prob_cat
+    prob_cont = model_cont.predict_log_proba(x_test_cont)
+    prob_cat = model_cat.predict_log_proba(x_test_cat)
 
+    new_features = np.hstack((prob_cat, prob_cont))
 
-    y_pred = TODO
+    new_model = GaussianNB()
+    new_model.fit(new_features)
+
+    y_pred = 1
 
     # Model accuracy
     accuracy = accuracy_score(y_pred, y_test)
