141b4c27489db8899fc8f2dc2ca01ae79a807fdd
Lukas <e11911069@student.tuwien.ac.at>
2023-11-10 17:57:54 +0100

    Simple version of Naive Bayes that works not well for Loan.

--- Ex1/Loan/Naive Bayes.py
+++ Ex1/Loan/Naive Bayes.py
@@ -24,67 +24,25 @@ def classify():
     del features[91]
     del features[0]
     x = loan_lrn[features]
+    # do one-hot encoding for all categorical variables (sklearn cannot handle categorical variables),
+    x = pd.get_dummies(x, drop_first=True)  # it would require some defined order for every categorical variable!
+    features = x.columns.to_list()
     y = loan_lrn.grade
-
-    numeric_features = x.drop(
-        ['term', 'emp_length', 'home_ownership', 'verification_status', 'loan_status',
-         'pymnt_plan', 'purpose', 'addr_state', 'initial_list_status', 'application_type',
-         'hardship_flag', 'debt_settlement_flag', 'disbursement_method'],
-        axis=1
-    ).columns.to_list()
-
-    categorical_features = ['term', 'emp_length', 'home_ownership', 'verification_status', 'loan_status',
-                            'pymnt_plan', 'purpose', 'addr_state', 'initial_list_status', 'application_type',
-                            'hardship_flag', 'debt_settlement_flag', 'disbursement_method']
-
-    # Label encode the categorical features (a label is a number)
-    x_cat = x[categorical_features]
-    le = LabelEncoder()
-    x_cat_encoded = x_cat.apply(le.fit_transform)
-    x_numeric_features = x[numeric_features]
-
-    # We have to reset the indices to be able to concatenate
-    x_cat_encoded.reset_index(drop=True, inplace=True)
-    x_numeric_features.reset_index(drop=True, inplace=True)
-    x = pd.concat([x_numeric_features, x_cat_encoded], axis=1)
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)  # 70% train, 30% test
 
-    # Based on this: https://stackoverflow.com/questions/14254203/mixing-categorial-and-continuous-data-in-naive-bayes-classifier-using-scikit-lea
-    # Build a Gaussian Classifier for continuous data
-    model_cont = GaussianNB()
-    # get the subset of the training set containing only continuous/numeric values
-    x_train_cont = x_train[numeric_features]
-
-    # Build a Categorical Classifier for categorical data
-    model_cat = MultinomialNB()
-    # get the subset of the training set containing only categorical values
-    x_train_cat = x_train[categorical_features]
+    # Create Naive Bayes classifier
+    model = GaussianNB()
 
     start = time.time()
-    # Model training continuous
-    model_cont.fit(x_train_cont, y_train)
-    # Model training categorical
-    model_cat.fit(x_train_cat, y_train)
+
+    # Train the model
+    model.fit(x_train, y_train)
 
     end = time.time()
     elapsed_time = end - start
 
-    # Predict Output
-    # Split input data
-    x_test_cont = x_test[numeric_features]
-    x_test_cat = x_test[categorical_features]
-
-    # Predict the log probabilities for both models
-    prob_cont = model_cont.predict_log_proba(x_test_cont)
-    prob_cat = model_cat.predict_log_proba(x_test_cat)
-
-    new_features = np.hstack((prob_cat, prob_cont))
-
-    new_model = GaussianNB()
-    new_model.fit(new_features)
-
-    y_pred = 1
+    y_pred = model.predict(x_test)
 
     # Model accuracy
     accuracy = accuracy_score(y_pred, y_test)
