ef51912ec8eba372d044a7394ab1d35518b5a35c
JohannesPfennigbauer <jpfennigbauer@gmx.at>
2023-11-11 17:20:40 +0100

    Updated Ozone preprocessing

---
+++ Ex1/Ozone Layer/DecisionTree.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+df = pd.read_pickle('data_preprocessed.pkl')
+
--- Ex1/Ozone Layer/Preprocessing.py
+++ Ex1/Ozone Layer/Preprocessing.py
@@ -2,13 +2,13 @@ import pandas as pd
 import numpy as np
 
 # Get column names from onehr.names
-names = pd.read_csv("onehr.names",
+names = pd.read_csv('../datasets/Ozone/onehr.names',
                     names=['name'])
 names['name'] = names['name'].str.split(':').str[0]
 list_of_names = names['name'].tolist()
 
 # Load Data
-df = pd.read_csv('../../datasets/onehr.data',
+df = pd.read_csv('../datasets/Ozone/onehr.data',
                  names=list_of_names,
                  parse_dates=['Date'])
 
@@ -24,7 +24,7 @@ for i, column in enumerate(df_str.columns):
 # Based on the data structure (numerical hourly weather data) we first thought
 # that linear interpolation would be a good choice to handle missing data.
 # However, since there are roughly 200 consecutive datapoints missing
-# linear interpolation as well as ffill makes no sense. Therefore, we decided to drop the missing data.
+# linear interpolation as well as ffill might not be the best choice. Therefore, we decided to drop the missing data.
 df = df.replace('?', np.NaN)
 df = df.dropna()
 
