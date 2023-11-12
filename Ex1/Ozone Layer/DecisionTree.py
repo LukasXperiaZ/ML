091b4421138d1255b9f557646d7dbc86c7712c06
JohannesPfennigbauer <jpfennigbauer@gmx.at>
2023-11-12 18:19:31 +0100

    Added DT and NN

--- Ex1/Ozone Layer/DecisionTree.py
+++ Ex1/Ozone Layer/DecisionTree.py
@@ -1,9 +1,46 @@
+import time
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+# for plotting the DT
+from six import StringIO
+from IPython.display import Image
+from sklearn.tree import export_graphviz
+import pydotplus
 
 df = pd.read_pickle('../../datasets/Ozone/data_preprocessed.pkl')
 
-print(df.head())
+X = df.drop(['Date','Class'], axis = 1)
+y = df.Class
 
+# Split dataset into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+
+# Create Decision Tree classifier object
+clf = DecisionTreeClassifier(criterion = 'gini')
+
+start = time.time()
+# Train Decision Tree Classifier
+clf = clf.fit(X_train,y_train)
+end = time.time()
+duration = end - start
+
+# Predict response for training and test dataset
+pred_test = clf.predict(X_test)
+
+# Model Accuracy, how often is the classifier correct?
+print(f"Duration: {round(duration,2)}s")
+print(metrics.classification_report(y_test, pred_test))
+cm = metrics.confusion_matrix(y_test, pred_test)
+disp = metrics.ConfusionMatrixDisplay(confusion_matrix=cm)
+disp.plot().figure_.savefig('Confusion_matrix_Ozone_DT.png')
+
+# Plot decision tree
+dot_data = StringIO()
+export_graphviz(clf, out_file=dot_data,
+                filled=True, rounded=True,
+                special_characters=True, feature_names = X.columns.to_list(),class_names=['0','1'])
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_png('DecisionTree_Ozone.png')
+Image(graph.create_png())
---
+++ Ex1/Ozone Layer/NN.py
@@ -0,0 +1,32 @@
+import time
+import pandas as pd
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+df = pd.read_pickle('../../datasets/Ozone/data_preprocessed.pkl')
+
+X = df.drop(['Date','Class'], axis = 1)
+y = df.Class
+
+# Split dataset into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+
+# Create Decision Tree classifier object
+mlp = MLPClassifier(hidden_layer_sizes= (74,74,74,74,74), activation='identity', solver='adam')
+
+start = time.time()
+# Train Decision Tree Classifier
+mlp = mlp.fit(X_train,y_train)
+end = time.time()
+duration = end - start
+
+# Predict response for training and test dataset
+pred_test = mlp.predict(X_test)
+
+# Model Accuracy, how often is the classifier correct?
+print(f"Duration: {round(duration,2)}s")
+print(metrics.classification_report(y_test, pred_test))
+cm = metrics.confusion_matrix(y_test, pred_test)
+disp = metrics.ConfusionMatrixDisplay(confusion_matrix=cm)
+disp.plot().figure_.savefig('Confusion_matrix_Ozone_NN.png')
