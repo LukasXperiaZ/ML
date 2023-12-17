30704b845c71df41afa6403a394eec571483b214
Adam Skuta <adam.skuta@gmail.com>
2023-12-17 11:07:32 +0100

    Added grid search (needs testing)

---
+++ Ex2/grid_search.py
@@ -0,0 +1,48 @@
+from itertools import permutations
+
+import pandas as pd
+from sklearn.model_selection import cross_val_score
+from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+
+class GridSearchMLP:
+    def __init__(self, params: dict):
+        self.params: dict = params
+        self.best_score: float = 0.0
+        self.best_params: dict = {}
+        self.best_model: Pipeline = None
+
+    
+    def find_params(self, X, y):
+        for nr_hidden_layers in self.params["nr_hidden_layers"]:
+            for hidden_layer_sizes in permutations(self.params["nr_neurons"], r=nr_hidden_layers):
+                for activation in self.params["activation"]:
+                    for solver in self.params["solver"]:
+                        preprocessor = ColumnTransformer(
+                            transformers=[
+                                ("scaler", StandardScaler(), X.select_dtypes(include="float64").columns),
+                                ("onehot", OneHotEncoder(handle_unknown="ignore"), X.select_dtypes(include="object").columns)
+                            ]
+                        )
+                        mlp = MLPClassifier(hidden_layer_sizes=hidden_layer_sizes,
+                                            activation=activation,
+                                            solver=solver)
+                        pipe = Pipeline(steps=[
+                            ('pre', preprocessor),
+                            ('mlpc', mlp)
+                        ])
+                        scores = cross_val_score(pipe, X, y, cv=5, scoring="accuracy")
+                        if scores.mean() > self.best_score:
+                            self.best_score = scores.mean()
+                            self.best_params = {
+                                "hidden_layer_sizes": hidden_layer_sizes,
+                                "activation": activation,
+                                "solver": solver
+                            }
+                            self.best_model = pipe
+                        
+                            print(f"Found new best parameters: {hidden_layer_sizes}, {activation}, {solver}")
+        
